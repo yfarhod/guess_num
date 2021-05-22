a319f98afea32e0e79cda97cb95ce9b113346319
unknown <yfarhod75@gmail.com>
2021-05-22 22:07:20 +0200

    header component updated

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View >
-      
+    <View style={styles.screen} >
+      <Header title="Guess a Number" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    flex: 1
+  }
 });
--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,26 @@ import { View, Text } from 'react-native';
 
 const Header = props => {
     return (
-        <View>
-            <Text>{props.title}</Text>
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>{props.title}</Text>
         </View>
     )
 
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 90,
+        paddingTop: 36,
+        backgroundColor: '#f7287b',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerTitle: {
+        color: 'black',
+        fontSize: 18
+    }
+});
 
 export default Header;
