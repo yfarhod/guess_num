4f30f3a8ea796a4df9bd4211692a7ffd04040689
unknown <yfarhod75@gmail.com>
2021-05-31 22:52:54 +0200

    startgamescreen component update

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -32,7 +32,12 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white'
     },
     title: {
         fontSize: 20,
