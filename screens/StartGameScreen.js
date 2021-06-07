1ac0e8321e2453b287f9be68a94cf0b24a6f1229
unknown <yfarhod75@gmail.com>
2021-06-07 15:01:02 +0200

    buttons handler added

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,10 +6,33 @@ import Colors from '../constants/colors';
 
 const StartGameScreen = (props) => {
 
-    [enteredValue, setEnteredValue] = useState('');
+    const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState();
 
     const numberInputHandler = inputText => {
         setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+    }
+
+    const resetInputHandler = () => {
+        setEnteredValue('');
+        setConfirmed(false);
+    };
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+            return;
+        }
+        setConfirmed(true);
+        setSelectedNumber(chosenNumber);
+        setEnteredValue('');
+    };
+
+    let confirmedOutput;
+
+    if (confirmed) {
+        confirmedOutput = <Text>Chosen Number: {selectedNumber} </Text>
     }
 
     return (
@@ -28,10 +51,11 @@ const StartGameScreen = (props) => {
                         onChangeText={numberInputHandler}
                         value={enteredValue} />
                     <View style={styles.buttonContainer}>
-                        <View style={styles.button}><Button title="Reset" onPress={() => { }} color={Colors.accent} /></View>
-                        <View style={styles.button}><Button title="Confirm" onPress={() => { }} color={Colors.primary} /></View>
+                        <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color={Colors.accent} /></View>
+                        <View style={styles.button}><Button title="Confirm" onPress={confirmInputHandler} color={Colors.primary} /></View>
                     </View>
                 </Card>
+                {confirmedOutput}
             </View>
         </TouchableWithoutFeedback>
     )
